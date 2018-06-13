8e197cac1e64dabfcf9bfc38ab191bb43e0c387e
kemerovo_man <kemerovo_man@mail.ru>
2018-06-13 17:12:34 +0700

    fix

--- src/Config/log.php
+++ src/Config/log.php
@@ -2,8 +2,8 @@
 
 return [
 //    'middleware' => ['web', 'isAdmin'],
-//     'storeDays' => 30
-//     'disable' => env('LOG_DISABLE', false)
-//    'format' => "[%datetime%] %message%\n",
+//    'storeDays' => 30
+//    'disable' => env('LOG_DISABLE', false)
+//    'format' => "[%datetime%] %message%\n"
 //    'dateFormat' => 'd/M/Y:H:i:s O'
 ];
