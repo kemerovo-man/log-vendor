b8d0aad077a0059eec7d4b51d48b7445a97d8383
kemerovo_man <kemerovo_man@mail.ru>
2019-01-17 13:16:35 +0700

    magic config

--- src/Facade/LogExample.php
+++ src/Facade/LogExample.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Log
  * @method static void testLog(string $message, int $parameter1)
+ *
+ * @method static void emergency(string $message, array $context = [])
+ * @method static void alert(string $message, array $context = [])
+ * @method static void critical(string $message, array $context = [])
+ * @method static void error(string $message, array $context = [])
+ * @method static void warning(string $message, array $context = [])
+ * @method static void notice(string $message, array $context = [])
+ * @method static void info(string $message, array $context = [])
+ * @method static void debug(string $message, array $context = [])
+ * @method static void log($level, string $message, array $context = [])
  */
 class LogExample extends Facade
 {
