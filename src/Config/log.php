f8524b1bb671bad60e7ca5c5ab1a2e0a2d85a453
kemerovo_man <kemerovo_man@mail.ru>
2019-01-18 15:13:45 +0700

    facades

--- src/Config/log.php
+++ src/Config/log.php
@@ -6,6 +6,6 @@ return [
 //    'disable' => env('LOG_DISABLE', false)
 //    'format' => "[%datetime%] %message%\n"
 //    'dateFormat' => 'd/M/Y:H:i:s O'
-//    'facade' => \App\Facades\Log::class
+//    'facades' => [\App\Facades\Log::class]
 
 ];
